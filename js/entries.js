176406fbec2ad2a392e7d74622836cd9973b371f
Lex Myers <lex.myers@gmail.com>
2016-02-01 17:01:57 -0800

    template invisible; more generate code; one more entry

--- js/entries.js
+++ js/entries.js
@@ -1,21 +1,21 @@
 entryData = [
   {
-    title:        'Kitty Counter',
+    sitename:        'Kitty Counter',
     siteUrl:      'http://icarrera.github.io/kitty-counter/',
     description:  '<p>Because I strongly believe that there are not nearly enough cats on the Internet.</p><p>Meow.</p>'
   },
   {
-    title:        'Bus Mall',
+    sitename:        'Bus Mall',
     siteUrl:      'http://lexm.github.io/bus-mall-marketing/',
     description:  '<p>A paradise ambassador all i need is all i need is two white horses in a line taking and the heat wave\'s calling your name and their iron colored ships and we will rise in the cool of the evening awaken primal feeling grinding down diamonds i can\'t think of what they\'re for oh they ruin me every time it\'s a grudge pull a hammer from a coal mine put it together, it\'s a strange invitation row me to the shore she took me off my guard, takin me far tell me who\'s that ridin, john the revelator to keep it warm at night turn yourself over you keep to yourself, stirring the dregs, where i have laid.</p>'
   },
   {
-    title:        'Campfire Coffee',
+    sitename:        'Campfire Coffee',
     siteUrl:      'http://lexm.github.io/campfire-coffee/',
     description:  '<p>And i shut my mouth before i even breathe and you stare into space trying to discern what to say now give me some sympathy he was only a person i had to set it on fire in vat of chicken fat i\'ll be twisting myself again novacane novacane red tape rivals shoot out all the traffic lights that\'s how long it took the ticker tape feeds the mind two dimensions, dumb your head down where do they go? would this be a world premiere?.</p>'
   },
   {
-    title:        'Guessing Game',
+    sitename:        'Guessing Game',
     siteUrl:      'http://lexm.github.io/guessing-game',
     description:  '<p>Come true feelings feelings feelings go against headwind or ride on the talewind. he was so wretched in the jungle lands it\'s like a big empty city full of toasters like a ghost town i\'ve found looking for a home ozzy ozzy ozzy ozzy ozzy ozzy ozzy ozzy takin me far the bones of our elders the microphone is in flames upon the funeral fire and sing it again? vietnam vet playin\' air guitar waiting to come down, down on the scene when the fuse is up who shattered themselves.</p>'
   }
--- js/generate.js
+++ js/generate.js
@@ -1,4 +1,4 @@
-var Entries = [];
+var entries = [];
 
 function Entry (opts) {
   this.sitename = opts.sitename;
@@ -10,11 +10,22 @@ Entry.prototype.toHtml = function() {
   var $newEntry = $('article.template').clone();
   // var entryTitle = $newEntry.find('#site');
   // entryTitle.text(this.sitename);
-  $newEntry.find('#site').text(this.sitename);
-  $newEntry.find('#site > a').attr('href, this.siteUrl');
+  var newSiteLink = '<a href="' + this.siteUrl + '">' + this.sitename + '</a>';
+  console.log(newSiteLink);
+  $newEntry.find('site').html(newSiteLink);
+  // $newEntry.find('#site').text(this.sitename);
+  // $newEntry.find('#site > a').attr('href, this.siteUrl');
   $newEntry.find('#site-desc').html(this.description);
 
   $newEntry.append('<hr>');
   $newEntry.removeClass('template');
   return $newEntry;
 }
+
+entryData.forEach(function(ele) {
+  entries.push(new Entry(ele));
+});
+
+entries.forEach(function(a) {
+  $('#entries').append(a.toHtml())
+});
