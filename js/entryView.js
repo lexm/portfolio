31b9863935f18a95b680b5206d21418c5394fa5f
Lex Myers <lex.myers@gmail.com>
2016-02-08 19:40:23 -0800

    added entryView.create

--- js/entryView.js
+++ js/entryView.js
@@ -9,6 +9,17 @@ entryView.handleMainNav = function() {
   });
 };
 
+entryView.create = function() {
+  var entry;
+  $('#entries').empty();
+  entry = new Entry({
+    sitename: $('#entry-sitename').val(),
+    siteUrl: $('#entry-siteurl').val(),
+    description: $('#entry-description').val()
+  });
+  $('#entries').append(entry.toHtml());
+};
+
 entryView.initIndexPage = function() {
   Entry.all.forEach(function(a) {
     $('#entries').append(a.toHtml());
