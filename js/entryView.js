7208f627d0e426ba9e64061e6eef4724bd47e2b6
Lex Myers <lex.myers@gmail.com>
2016-02-09 19:32:46 -0800

    enclosed generate.js in IIFE

--- js/entryView.js
+++ js/entryView.js
@@ -1,33 +1,41 @@
-var entryView = {};
+(function(module) {
 
-entryView.handleMainNav = function() {
-  $('.main-nav').on('click', '.tab', function() {
-    var $tabZone = $('.tab-zone');
-    var $dataZone = $(this).data('zone');
-    $tabZone.hide();
-    $('#' + $dataZone).fadeIn();
-    // $tabZone.filter('#' + $dataZone).show();
+  var entryView = {};
+
+  entryView.handleMainNav = function() {
+    $('.main-nav').on('click', '.tab', function() {
+      var $tabZone = $('.tab-zone');
+      var $dataZone = $(this).data('zone');
+      $tabZone.hide();
+      $('#' + $dataZone).fadeIn();
+      // $tabZone.filter('#' + $dataZone).show();
+    });
+  };
+
+  entryView.create = function() {
+    var entry;
+    $('#entries').empty();
+    entry = new Entry({
+      sitename: $('#entry-sitename').val(),
+      siteUrl: $('#entry-siteurl').val(),
+      description: $('#entry-description').val()
+    });
+    $('#entries').append(entry.toHtml());
+
+  };
+
+  entryView.initIndexPage = function() {
+    Entry.all.forEach(function(a) {
+      $('#entries').append(a.toHtml());
+    });
+  };
+
+  $(function() {
+    entryView.handleMainNav();
   });
-};
 
-entryView.create = function() {
-  var entry;
-  $('#entries').empty();
-  entry = new Entry({
-    sitename: $('#entry-sitename').val(),
-    siteUrl: $('#entry-siteurl').val(),
-    description: $('#entry-description').val()
-  });
-  $('#entries').append(entry.toHtml());
+  module.entryView = entryView;
 
-};
 
-entryView.initIndexPage = function() {
-  Entry.all.forEach(function(a) {
-    $('#entries').append(a.toHtml());
-  });
-};
 
-$(function() {
-  entryView.handleMainNav();
-});
+})(window);
--- js/generate.js
+++ js/generate.js
@@ -1,59 +1,64 @@
-var entries = [];
+(function(module) {
 
-function Entry (opts) {
-  this.sitename = opts.sitename;
-  this.siteUrl = opts.siteUrl;
-  this.description = opts.description;
-}
+  var entries = [];
 
-Entry.all = [];
-
-Entry.prototype.toHtml = function() {
-  var template = Handlebars.compile($('#entry-template').text());
-  return template(this);
-};
-
-Entry.loadAll = function(rawData) {
-  rawData.forEach(function(ele) {
-    Entry.all.push(new Entry(ele));
-  });
-};
-
-Entry.getData = function() {
-  console.log('where am i');
-  $.getJSON('data/entries.json', function(data) {
-    console.log('data is ' + data);
-    localStorage.rawEntryData = JSON.stringify(data);
-    Entry.loadAll(data);
-    entryView.initIndexPage();
-  });
-};
-
-Entry.fetchAll = function () {
-  if(localStorage.rawEntryData) {
-    $.ajax({
-      type: 'HEAD',
-      url: 'data/entries.json',
-      success: function(data, message, xhr) {
-        var eTag = xhr.getResponseHeader('eTag');
-        if (!localStorage.eTag || eTag !== localStorage.eTag) {
-          localStorage.eTag = eTag;
-          Entry.getData();
-        } else {
-          Entry.loadAll(JSON.parse(localStorage.rawEntryData));
-          entryView.initIndexPage();
-        }
-      }
-    });
-  } else {
-    Entry.getData();
+  function Entry (opts) {
+    this.sitename = opts.sitename;
+    this.siteUrl = opts.siteUrl;
+    this.description = opts.description;
   }
-};
 
-// entries.forEach(function(a) {
-//   $('#entries').append(a.toHtml());
-// });
+  Entry.all = [];
 
-entryView.initIndexPage();
+  Entry.prototype.toHtml = function() {
+    var template = Handlebars.compile($('#entry-template').text());
+    return template(this);
+  };
 
-Entry.fetchAll();
+  Entry.loadAll = function(rawData) {
+    rawData.forEach(function(ele) {
+      Entry.all.push(new Entry(ele));
+    });
+  };
+
+  Entry.getData = function() {
+    console.log('where am i');
+    $.getJSON('data/entries.json', function(data) {
+      console.log('data is ' + data);
+      localStorage.rawEntryData = JSON.stringify(data);
+      Entry.loadAll(data);
+      entryView.initIndexPage();
+    });
+  };
+
+  Entry.fetchAll = function () {
+    if(localStorage.rawEntryData) {
+      $.ajax({
+        type: 'HEAD',
+        url: 'data/entries.json',
+        success: function(data, message, xhr) {
+          var eTag = xhr.getResponseHeader('eTag');
+          if (!localStorage.eTag || eTag !== localStorage.eTag) {
+            localStorage.eTag = eTag;
+            Entry.getData();
+          } else {
+            Entry.loadAll(JSON.parse(localStorage.rawEntryData));
+            entryView.initIndexPage();
+          }
+        }
+      });
+    } else {
+      Entry.getData();
+    }
+  };
+  module.Entry = Entry;
+
+  // entries.forEach(function(a) {
+  //   $('#entries').append(a.toHtml());
+  // });
+
+  // entryView.initIndexPage();
+
+  // Entry.fetchAll();
+
+})(window);
