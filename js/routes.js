03935f3a8ea991e3f4dfd551bb64a229768592f5
Lex Myers <lex.myers@gmail.com>
2016-02-18 15:43:15 -0800

    renamed generate.js to portfolio; added /portfolio route

--- js/routes.js
+++ js/routes.js
@@ -1,4 +1,6 @@
 page('/', entryController.index);
 page('/about', aboutController.index);
 page('/repos', repoController.index);
+page('/portfolio', '/');
+// page('/portfolio/:portEntryName', );
 page();
